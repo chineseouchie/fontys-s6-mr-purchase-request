518c39c709284819f4037cfcf5620388c2c8b9a1
Niek <n.moor@student.fontys.nl>
2022-04-08 09:11:22 +0200

    Auto stash before merge of "MRS6-120" and "origin/mrs6-57"
    .

--- src/main/java/com/mobility/purchaserequest/PurchaseRequestApplication.java
+++ src/main/java/com/mobility/purchaserequest/PurchaseRequestApplication.java
@@ -1,12 +1,16 @@
 package com.mobility.purchaserequest;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PurchaseRequestApplication {
+public class PurchaseRequestApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PurchaseRequestApplication.class, args);
 	}
+
+	
+
 }
